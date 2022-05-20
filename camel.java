a1599235880ed8e38dbeb2490be74fa6c1cbf480
Harishan <335677076@gapps.yrdsb.ca>
2022-05-19 21:01:40 -0400

    part2 video tutorial

--- camel.java
+++ camel.java
@@ -14,16 +14,63 @@ public class Camel extends Actor
      */
     
     GreenfootSound camelSound = new GreenfootSound("camel6.mp3");
+    GreenfootImage[] camelRunRight = new GreenfootImage[12];
+    GreenfootImage[] camelRunLeft = new GreenfootImage[12];
+    
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer(); 
+    
+    public Camel(){
+        for(int i=0;i<camelRunRight.length;i++){
+           camelRunRight[i] = new GreenfootImage("run"+(i+1));  
+           camelRunRight[i].scale(50,50);
+        }
+        
+        
+        for(int i=0;i<camelRunLeft.length;i++){
+           camelRunLeft[i] = new GreenfootImage("run"+(i+1)); 
+           camelRunLeft[i].mirrorHorizontally();
+           camelRunLeft[i].scale(50,50);
+        }
+        
+        animationTimer.mark();
+        setImage(camelRunRight[0]);
+    }
+    
+    /** animate Camel 
+     * 
+     * 
+     */
+    int imageIndex = 0;
+    public void animateCamel(){
+        if(animationTimer.millisElapsed()<100){
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right")){
+            setImage(camelRunRight[imageIndex]);
+            imageIndex= (imageIndex+1) % camelRunRight.length;
+        }
+        else{
+            setImage(camelRunLeft[imageIndex]);
+            imageIndex= (imageIndex+1) % camelRunLeft.length;
+        }
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("a")){
             move(-2);
+            facing = "left";
         }
         if(Greenfoot.isKeyDown("d")){
             move(2);
+            facing = "right";
         }
         
         eat();
+        
+        animateCamel();
     }
     
     public void eat()
